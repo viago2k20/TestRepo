331e5e84475709048bd875948ab1bda9cd6710fd
Denis <denzh@mail.ru>
2023-06-21 01:25:55 +0300

    add One.java and Two.java

---
+++ io/twofiles/One.java
@@ -0,0 +1,7 @@
+package io.twofiles;
+
+public class One {
+  public static void main(String[] args) {
+    Two.greeting();
+  }
+}
---
+++ io/twofiles/Two.java
@@ -0,0 +1,7 @@
+package io.twofiles;
+
+public class Two {
+  public static void greeting() {
+    System.out.println("Java fo Brave!");
+  }
+}
